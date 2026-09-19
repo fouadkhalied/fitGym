a29261de047b4a55653e91b157a938b3976aa3b7
fouadkhalied <foukha49@gmail.com>
2026-09-19 07:50:07 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_DATABASE_URL: process.env.DATABASE_URL,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
